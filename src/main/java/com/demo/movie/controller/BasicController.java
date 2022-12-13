8068ea1e925c0f0844de6e60b2483b34cbdbde93
khuongduy99 <78532639+khuongduy99@users.noreply.github.com>
2022-12-14 05:13:25 +0700

    cap nhat source

---
+++ src/main/java/com/demo/movie/controller/BasicController.java
@@ -0,0 +1,19 @@
+package com.demo.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BasicController {
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about() {
+		return "about";
+	}
+}
